ac9d6a3fdb0acd81553b4c1339f83c2b4333b7b9
Angel Escaño <160737093+JustBeWell@users.noreply.github.com>
2025-06-04 19:01:37 +0200

    .

--- ProyectoMant/src/main/java/es/taw/primerparcial/dao/AlbumRepository.java
+++
@@ -1,7 +0,0 @@
-package es.taw.primerparcial.dao;
-
-import es.taw.primerparcial.entity.Album;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlbumRepository extends JpaRepository<Album, Integer> {
-}
